93c14f2a33cd10ea5cddb6d7eb496ce194625a39
aziztraorer <aziztraore55801@gmail.com>
2026-07-15 01:21:58 +0000

    test api route

--- HomeLinka/api/index.js
+++ HomeLinka/api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "dns";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -21,3 +22,5 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
   });
+
+app.use("/api/user", userRoutes );
