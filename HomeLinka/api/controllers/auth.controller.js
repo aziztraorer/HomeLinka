e5f19e9bf6c294e877782d461a2fe504a8a8273c
aziztraorer <aziztraore55801@gmail.com>
2026-07-24 14:10:23 +0000

    creation de middleware et de fonction d'erreur

--- HomeLinka/api/controllers/auth.controller.js
+++ HomeLinka/api/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 
-
-export const signup =async (req, res) =>{
+export const signup =async (req, res ,next) =>{
 
    const { username, email, password } = req.body;
    const hashedPassword = await bcrypt.hash(password, 10);
@@ -13,6 +13,6 @@ export const signup =async (req, res) =>{
       res.status(201).json({ message: "User created successfully" });
    }
     catch (error) {
-      res.status(500).json({ error: error.message });
+      next(error);
     }
 }
--- HomeLinka/api/index.js
+++ HomeLinka/api/index.js
@@ -27,3 +27,13 @@ mongoose
 
 app.use("/api/user", userRoutes );
 app.use("/api/auth", authRoutes );
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ 
+    sussess: false,
+    statusCode,
+    message: message
+  });
+})
